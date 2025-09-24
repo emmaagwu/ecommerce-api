9169fc18a2866c3734faeffb458be74a91510c57
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-09-24 14:09:28 +0100

    Refactor superuser creation in migration to use User model and set password securely

--- accounts/migrations/0002_create_initial_superuser.py
+++ accounts/migrations/0002_create_initial_superuser.py
@@ -1,13 +1,39 @@
+# from django.db import migrations
+
+# def create_superuser(apps, schema_editor):
+#     User = apps.get_model('accounts', 'User')
+#     if not User.objects.filter(email='admin@example.com').exists():
+#         User.objects.create_superuser(
+#             full_name='Agwu Emmanuel',
+#             email='emmanuelagwu89@gmail.com',
+#             password='eueacj12345' 
+#         )
+
+# class Migration(migrations.Migration):
+
+#     dependencies = [
+#         ('accounts', '0001_initial'),
+#     ]
+
+#     operations = [
+#         migrations.RunPython(create_superuser),
+#     ]
+
+
 from django.db import migrations
 
 def create_superuser(apps, schema_editor):
     User = apps.get_model('accounts', 'User')
     if not User.objects.filter(email='admin@example.com').exists():
-        User.objects.create_superuser(
+        user = User(
             full_name='Agwu Emmanuel',
             email='emmanuelagwu89@gmail.com',
-            password='eueacj12345' 
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
         )
+        user.set_password('eueacj123456')
+        user.save()
 
 class Migration(migrations.Migration):
 
