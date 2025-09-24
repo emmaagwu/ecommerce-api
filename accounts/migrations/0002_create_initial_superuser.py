5027dcb74b66043e0725988027b086193afde2f7
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-09-24 14:16:40 +0100

    Update superuser creation in migration to use consistent email and hashed password

--- accounts/migrations/0002_create_initial_superuser.py
+++ accounts/migrations/0002_create_initial_superuser.py
@@ -21,18 +21,19 @@
 
 
 from django.db import migrations
+from django.contrib.auth.hashers import make_password
 
 def create_superuser(apps, schema_editor):
     User = apps.get_model('accounts', 'User')
     if not User.objects.filter(email='admin@example.com').exists():
         user = User(
-            full_name='Agwu Emmanuel',
-            email='emmanuelagwu89@gmail.com',
+            email='admin@example.com',
+            full_name='Admin User',
             is_staff=True,
             is_superuser=True,
             is_active=True,
+            password=make_password('eueacj123456'),
         )
-        user.set_password('eueacj123456')
         user.save()
 
 class Migration(migrations.Migration):
