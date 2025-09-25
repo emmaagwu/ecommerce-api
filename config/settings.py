59e0bff802742b375c7a8bfb5b661b62a6b9a5aa
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-09-25 12:03:43 +0100

    Add CORS support and update requirements for django-cors-headers

--- config/settings.py
+++ config/settings.py
@@ -45,11 +45,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts',
     'catalog',
+    'corsheaders',
     'drf_spectacular',
 ]
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -82,6 +84,12 @@ TEMPLATES = [
         },
     },
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
